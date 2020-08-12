8323ab049e6696ac97a064181bc17cbb52265dd1
CodeSalvageON <alexy0730@gmail.com>
2020-08-12 19:55:28 +0000

    Added Dictionary Definitions

--- index.js
+++ index.js
@@ -12,11 +12,62 @@ app.use(bodyParser.json());
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 
+//Setting Dictionary Directory
+const dictionary_directory = __dirname+'/public/dictionary/';
+const dictionary_head = `
+<!DOCTYPE html>
+<html>
+  <head>
+    <!-- Favicon -->
+    <link rel="icon" href="https://vignette.wikia.nocookie.net/starwars/images/d/de/Republic_Emblem.svg/revision/latest/scale-to-width-down/340?cb=20080311202148" />
+
+    <!-- Local CSS Files -->
+    <link rel="stylesheet" href="https://tgrdictionary.codesalvageon.repl.co/styles/style.css" />
+
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Titillium+Web&display=swap" rel="stylesheet">
+  </head>
+  <body>
+`;
+const dictionary_footer = `
+  </body>
+</html>
+`;
+
+var def_list = ``;
+
 //Static
 app.get('/', function(req, res){
   res.sendFile(__dirname+'/public/static/index.html');
 });
 
+app.get('/dictionary', function(req, res){
+  res.sendFile(__dirname+'/public/static/dictionary.html');
+});
+
+app.get('/write', function(req, res){
+  res.sendFile(__dirname+'/public/static/write.html');
+});
+
 http.listen(port, function(){
   console.log('listening on *:' + port);
+});
+
+//Dictionary Definitions 
+app.get('/definitions', function(req, res){
+  fs.readdir(dictionary_directory, (err, files) => {
+    if (err){
+      throw err;
+    }
+
+    files.forEach(file => {
+      var fixed_file_name = file.replace('public/dictionary/', "");
+      var repaired_file_name = file.replace('.html', "");
+
+      def_list = def_list + `<a href="https://tgrdictionary.codesalvageon.repl.co/`+file+`"><h2>`+repaired_file_name+`</h2></a>`;
+      console.log(def_list);
+    });
+  });
+
+  res.send(dictionary_head+def_list+dictionary_footer);
 });
---
+++ public/scripts/script.js
@@ -0,0 +1,12 @@
+//Set Redirect Button Clicks
+$("#dictionary-definitions").click(function(){
+  location = 'https://tgrdictionary.codesalvageon.repl.co/dictionary';
+});
+
+$("#write-definitions").click(function(){
+  location = 'https://tgrdictionary.codesalvageon.repl.co/write';
+});
+
+$("#dictionary-logo").click(function(){
+  location = 'https://tgrdictionary.codesalvageon.repl.co';
+});
